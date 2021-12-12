62d70d4fe362cffe66cde3b19f592af150771603
arielandriel <46574963+arielandriel@users.noreply.github.com>
2021-12-11 22:00:19 -0300

    Se ingresan los datos por un Form, y se guardan en un archivo Json, falta mostrar todos los visitantes y los que no son de la tierra.

--- contador.php
+++ contador.php
@@ -1,5 +1,80 @@
 <?php
 
+class Visita
+{
+    public $nombre;
+    public $planeta;
 
+    public function getNombre(){
+        return $this->nombre;
+    }
+    
+    public function setNombre($nombre){
+        $this->nombre = $nombre;
+    
+    }
+
+    public function getPlaneta(){
+        return $this->planeta;
+    }
+    
+    public function setPlaneta($planeta){
+        $this->planeta = $planeta;
+    
+    }
+
+function mostrarDatos()
+{
+    echo 'Tu nombre es: ';
+   echo $this->nombre;
+   echo '<br>';
+   echo 'Y vienes de:';
+   echo $this->planeta;
+}
+
+function guardar()
+{
+$a =$this->nombre;
+$b =$this->planeta;
+$new = array('Nombre'=>$a, 'Planeta' =>$b);
+if (file_exists('visitas.json'))
+{
+ $contenido = file_get_contents('visitas.json');
+ $data = json_decode($contenido);
+ array_push($data, $new);
+ file_put_contents('visitas.json', json_encode($data));
+
+}else{
+    $data = array();
+    array_push($data, $new);
+    $f = fopen('visitas.json', "w");
+    fwrite($f, json_encode($data));
+    fclose($f);
+}
+//header(("Location:index.php"));
+}
+
+}
+
+function mostrarVisitas()
+{
+    $contenido = file_get_contents('visitas.json');
+    $data = json_decode($contenido);
+    foreach($data as $indice=>$datos)
+    {
+        echo $datos;
+    }
+}
+
+if(isset($_POST['mostrar']))
+{
+    $nombre = $_POST['nombre'];
+    $planeta = $_POST['planeta'];
+    $vistaUno = new Visita();
+    $vistaUno->setnombre($nombre);
+    $vistaUno->setplaneta($planeta);
+    $vistaUno->guardar();
+    mostrarVisitas();
+}
 
 ?>
--- index.php
+++ index.php
@@ -5,9 +5,30 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"> 
-    <title>Contador</title>
+    <title>Contador Extraterrestre</title>
 </head>
 <body>
-    
+<form class="w3-container w3-card-4" action="contador.php" method="post">
+<p>
+<label for="cars">Ingrese sus datos</label>
+  
+  <input class="w3-input" type="text" name="nombre" placeholder="Nombre" required></p>
+  <p>
+  <label for="cars">Planeta Perteneciente</label>
+<select name="planeta" >
+  <option value="Marte">Marte</option>
+  <option value="Saturno">Saturno</option>
+  <option value="jupiter">Jupiter</option>
+  <option value="pluton">Plutón</option>
+  <option value="Tierra">Tierra</option>
+  <option value="Mercurio">Mercurio</option>
+  <option value="Venus">Venus</option>
+  <option value="Urano">Urano</option>
+  <option value="Neptuno">Neptuno</option>
+  <option value="Ceres">Ceres</option>
+  <option value="Eris">Eris</option>
+</select>
+<input type="submit" class="w3-button w3-khaki" name="mostrar">
+</form>
 </body>
 </html>
